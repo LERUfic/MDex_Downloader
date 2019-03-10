90394a4ecb54640e0b2e38a72979a5436ee3d964
Aguel Satria Wijaya <aguelsw@gmail.com>
2019-03-10 16:05:44 +0700

    change passing argument; generate folder

--- hanayome.py
+++ hanayome.py
@@ -1,5 +1,6 @@
-import sys
+import os
 import time
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -8,6 +9,7 @@ import configparser
 class MangaDex:
 	options = None
 	driver = None
+	manga_path = None
 
 	def __init__(self, driver_path):
 		try:
@@ -20,9 +22,12 @@ class MangaDex:
 			print("[FAILED]: Cannot starting chrome!")
 			exit()
 
-	def openManga(self,manga_path):
+	def setMangaPath(self,manga_path):
+		self.manga_path = manga_path
+
+	def openManga(self):
 		try:
-			self.driver.get(manga_path)
+			self.driver.get(self.manga_path)
 			time.sleep(6)
 
 			return self.driver.page_source
@@ -30,11 +35,11 @@ class MangaDex:
 			print("[FAILED]: Cannot opening MDex!")
 			exit()
 
-	def getAllPage(self,manga_path):
+	def getAllPage(self):
 		try:
 			halaman=1
 			while(1):
-				page_link = manga_path+"/chapters/"+str(halaman)
+				page_link = self.manga_path+"/chapters/"+str(halaman)
 				self.driver.get(page_link)
 				if "No results found." in self.driver.page_source:
 					return halaman-1
@@ -46,20 +51,61 @@ class MangaDex:
 			print("[FAILED]: Error opening page!")
 			exit()
 
-	def getAllChapter(self,manga_path,pages):
+	def getAllChapters(self,pages):
 		try:
-			allChapter = []
+			allChapters = []
 			for i in range(pages):
-				chapter_page = self.getPageChapter(manga_path,i+1)
-				allChapter = allChapter + chapter_page
-			return allChapter
+				chapter_page = self.getPageChapter(i+1)
+				allChapters = allChapters + chapter_page
+			return allChapters
 		except:
-			print("[FAILED]: Error getting all chapters")
+			print("[FAILED]: Error getting all chapters!")
 			exit()
 
-	def getPageChapter(self,manga_path,page):
+	def createMainFolder(self, allChapters):
 		try:
-			page_link = manga_path+"/chapters/"+str(page)
+			# folder = re.findall(r"\w+[0-9]",self.manga_path)
+			folder = self.manga_path.split("/")
+			main_folder = folder[-2]+"-"+folder[-1]
+			access_rights = 0o755
+			if(os.path.isdir(main_folder)):
+				print("Main Folder %s exist" % main_folder)
+			else:
+				try:
+					os.mkdir(main_folder, access_rights)
+				except OSError:
+					print ("[FAILED] Cannot create %s " % main_folder)
+				else:
+					print ("[SUCCESS] Created %s" % main_folder)
+
+			for i in range(len(allChapters)):
+				self.downloadImage(main_folder,allChapters[i])
+
+		except:
+			print("[FAILED]: Cannot create main folder!")
+			exit()
+
+	def downloadImage(self,base_folder,chapter):
+		try:
+			named_folder = chapter["chapter"]+" - "+chapter["title"]+" - "+chapter["id"]
+			chapter_folder = base_folder+"/"+named_folder
+			if(os.path.isdir(chapter_folder)):
+				print("Chapter Folder %s exist" % chapter_folder)
+			else:
+				try:
+					access_rights = 0o755
+					os.mkdir(chapter_folder, access_rights)
+				except OSError:
+					print ("[FAILED] Cannot create %s " % chapter_folder)
+				else:
+					print ("[SUCCESS] Created %s" % chapter_folder)
+		except:
+			print("[FAILED]: Cannot create chapter folder!")
+			exit()
+
+	def getPageChapter(self,page):
+		try:
+			page_link = self.manga_path+"/chapters/"+str(page)
 			self.driver.get(page_link)
 			soup = BeautifulSoup(self.driver.page_source,'html.parser')
 			info = soup.findAll("div", {"data-lang": "1"})
@@ -86,15 +132,19 @@ class MangaDex:
 
 
 def configRead():
-	configs = configparser.ConfigParser()
-	configs.read('config.ini')
-	config = {}
-	config['USERNAME'] = configs['USER']['USERNAME']
-	config['PASSWORD'] = configs['USER']['PASSWORD']
-	config['DRIVER_PATH'] = configs['LOCATION']['DRIVER_PATH']
-	config['MANGA_PATH'] = configs['LOCATION']['MANGA_PATH']
+	try:
+		configs = configparser.ConfigParser()
+		configs.read('config.ini')
+		config = {}
+		config['USERNAME'] = configs['USER']['USERNAME']
+		config['PASSWORD'] = configs['USER']['PASSWORD']
+		config['DRIVER_PATH'] = configs['LOCATION']['DRIVER_PATH']
+		config['MANGA_PATH'] = configs['LOCATION']['MANGA_PATH']
 
-	return config
+		return config
+	except:
+		print("[FAILED]: Cannot read config.ini")
+		exit()
 
 def main():
 	config = configRead()
@@ -103,17 +153,19 @@ def main():
 	myManga = MangaDex(config['DRIVER_PATH'])
 	print("[SUCCESS]: Opening chrome.")
 	
-	# print("Trying to open MDex...")
-	# # page = myManga.openManga(config['MANGA_PATH'])
-	# print("[SUCCESS]: Opening MDex.")
+	print("Trying to set Manga Url...")
+	myManga.setMangaPath(config['MANGA_PATH'])
+	print("[SUCCESS]: Set Manga Url.")
 
 	print("Searching how many pagination...")
-	pages = myManga.getAllPage(config['MANGA_PATH'])
+	pages = myManga.getAllPage()
 	print("[FOUND]: "+str(pages)+" pages")
 
 	print("Getting All Chapters...")
-	allChapters = myManga.getAllChapter(config['MANGA_PATH'],pages)
+	allChapters = myManga.getAllChapters(pages)
 	print("[SUCCESS]: Found "+str(len(allChapters))+" Chapters")
+
+	myManga.createMainFolder(allChapters)
 
 	print("Closing Chrome...")
 	myManga.closeDriver()

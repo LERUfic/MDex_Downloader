564363cd197382aebde93fbb1b4f06b773e7c0c2
Aguel Satria Wijaya <aguelsw@gmail.com>
2019-03-10 12:05:04 +0700

    Add searching how many pages

--- hanayome.py
+++ hanayome.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,19 +10,48 @@ class MangaDex:
 	driver = None
 
 	def __init__(self, driver_path):
-		self.options = webdriver.ChromeOptions()
-		# self.options.add_argument('--headless')
-		self.options.add_argument('--incognito')
-		# self.options.add_argument('window-size=1200x600')
-		self.driver = webdriver.Chrome(driver_path,options=self.options)
-	
+		try:
+			self.options = webdriver.ChromeOptions()
+			self.options.add_argument('--headless')
+			self.options.add_argument('--incognito')
+			self.options.add_argument('window-size=1200x600')
+			self.driver = webdriver.Chrome(driver_path,options=self.options)
+		except:
+			print("[FAILED]: Cannot starting chrome!")
+			exit()
+
 	def openManga(self,manga_path):
-		self.driver.get(manga_path)
-		time.sleep(2)
+		try:
+			self.driver.get(manga_path)
+			time.sleep(6)
+
+			return self.driver.page_source
+		except:
+			print("[FAILED]: Cannot opening MDex!")
+			exit()
+
+	def getAllPage(self,manga_path):
+		try:
+			halaman=1
+			while(1):
+				page_link = manga_path+"/chapters/"+str(halaman)
+				self.driver.get(page_link)
+				if "No results found." in self.driver.page_source:
+					return halaman-1
+					break
+				else:
+					print("Page "+str(halaman)+" Found")
+					halaman = halaman + 1
+		except:
+			print("[FAILED]:")
+			exit()
 
 	def closeDriver(self):
-		self.driver.quit()
-
+		try:
+			self.driver.quit()
+		except:
+			print("[FAILED]: Cannot closing chrome!")
+			exit()
 
 
 def configRead():
@@ -38,9 +68,23 @@ def configRead():
 def main():
 	config = configRead()
 
+	print("Trying to start chrome...")
 	myManga = MangaDex(config['DRIVER_PATH'])
-	myManga.openManga(config['MANGA_PATH'])
+	print("[SUCCESS]: Opening chrome.")
+	
+	# print("Trying to open MDex...")
+	# # page = myManga.openManga(config['MANGA_PATH'])
+	# print("[SUCCESS]: Opening MDex.")
+
+	print("Searching how many pagination...")
+	pages = myManga.getAllPage(config['MANGA_PATH'])
+	print("[FOUND]: "+str(pages)+" pages")
+
+
+
+	print("Closing Chrome...")
 	myManga.closeDriver()
+	print("[DONE]: Happy Reading.")
 
 
 if __name__ == '__main__':
